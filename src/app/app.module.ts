5750a25df45e562b631b2ce89459d60e160eef5d
Saba <sabatevdorashvili99@gmail.com>
2023-04-08 14:57:54 +0400

    =ad component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AdComponent } from './ad/ad.component';
+import { AdHeaderComponent } from './ad/ad-header/ad-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AdComponent,
+    AdHeaderComponent
   ],
   imports: [
     BrowserModule
